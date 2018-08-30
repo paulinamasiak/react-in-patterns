e23936488a3593a002a27a2342b48c1372e9274c
Paulina Masiak <paula.masiak@gmail.com>
2018-08-30 14:08:57 +0200

    Add comment

--- src/render-props-pattern/index.js
+++ src/render-props-pattern/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import List from './List';
 import data from './data.json';
 
+// Render prop is a function prop that a component uses to know what to render.
+// In this case a render prop is itemRenderer prop.
+
 const itemRederer = (item) => (
   <div className="list__item" key={item.id}>
     {item.name}
@@ -9,10 +12,10 @@ const itemRederer = (item) => (
 );
 
 const RenderPropsPattern = () => (
-  <div>
+  <section>
     <h1>Render props pattern example</h1>
     <List items={data} itemRenderer={itemRederer} />
-  </div>
+  </section>
 );
 
 export default RenderPropsPattern;
