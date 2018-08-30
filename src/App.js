3be608383acdf2574e0a81c1f6fd9471b9a61fca
Paulina Masiak <paulina.masiak@uxpin.com>
2018-08-30 14:12:52 +0200

    Reder props pattern (#1)
    * Add simple list (WIP)
    
    * Add comment
    
    * Refactoring

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import RenderPropsPattern from './render-props-pattern';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app">
+        <header className="app__header">
+          <img src={logo} className="app__logo" alt="logo" />
+          <h1 className="app__title">Welcome to React in Patterns</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main className="app__main">
+          <RenderPropsPattern />
+        </main>
       </div>
     );
   }
