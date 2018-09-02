4d73fb3eda8d09287ba149e8e8fcfe208a06a14d
Paulina Masiak <paula.masiak@gmail.com>
2018-09-02 15:07:44 +0200

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ControlledComponent from './controlled-component';
 import UncontrolledComponent from './uncontrolled-component';
 import RenderProps from './render-props';
 import HigherOrderComponent from './higher-order-component';
 import logo from './logo.svg';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <header className="app-header text-center">
-          <img src={logo} className="app-logo" alt="logo" />
-          <h1 className="app-title">Welcome to React in Patterns</h1>
-        </header>
-        <main>
-          <section className="app-section">
-            <ControlledComponent />
-          </section>
-          <section className="app-section">
-            <UncontrolledComponent />
-          </section>
-          <section className="app-section">
-            <RenderProps />
-          </section>
-          <section className="app-section">
-            <HigherOrderComponent />
-          </section>
-        </main>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="app">
+    <header className="app-header text-center">
+      <img src={logo} className="app-logo" alt="logo" />
+      <h1 className="app-title">Welcome to React in Patterns</h1>
+    </header>
+    <main>
+      <section className="app-section">
+        <ControlledComponent />
+      </section>
+      <section className="app-section">
+        <UncontrolledComponent />
+      </section>
+      <section className="app-section">
+        <RenderProps />
+      </section>
+      <section className="app-section">
+        <HigherOrderComponent />
+      </section>
+    </main>
+  </div>
+);
 
 export default App;
